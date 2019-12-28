2c21fd4fa656a9f0ea009f0d1b2101b1a5025ca3
AndreSynZ <45897999+AndreSynZ@users.noreply.github.com>
2019-12-28 21:31:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,12 +106,12 @@ bot.on("message", async message => {
 			.addField("Reactions", ":lizard: \n :monkey: \n :bug: \n :bee: \n :turkey: \n :sloth:",true) 
 			.setThumbnail("https://revivalservers.com/home/assets/media/logos/main.png") 
 		message.channel.send(myembed).then(async msg =>{
-			await msg.react("660579809436368929");
-			await msg.react("660579920006610964");
-			await msg.react("660579976210284596");
-			await msg.react("660580032544112660");
-			await msg.react("660580078157299762");
-			await msg.react("660580291366354944");
+			await msg.react(`🦎`);
+			await msg.react(`🐒`);
+			await msg.react(`🐛`);
+			await msg.react(`🐝`);
+			await msg.react(`🦃`);
+			await msg.react(`🦥`);
 		});
 		Timeout(Settings.Timeout);
 	}
