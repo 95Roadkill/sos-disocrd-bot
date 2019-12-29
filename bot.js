79df6665b5b03871554fcbb7f9d663f784c9d5a0
95Roadkill <Lt.roadkill@yahoo.co.uk>
2019-12-29 00:32:15 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -205,6 +205,7 @@ bot.on("message", async message => {
 		var PlayerScore = "```"
 		var PlayerTime = "```"
 		SQ.getPlayers(function(err, info){
+			console.log(info)
 			for(var i=0; i < info.length - 1; i++){
 				if (info[i].name != ""){
 					Players = Players + (i+1) + " : " + info[i].name + "\n";
